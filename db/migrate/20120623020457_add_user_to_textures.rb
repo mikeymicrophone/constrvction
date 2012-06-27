2df192ae1403689c1c766c378b5e35778ff4980d
Mary Huang <rhymeandreason1.0@gmail.com>
2012-06-26 23:53:11 -0400

    cleaned up db migrate, fixed some javascript tags

--- db/migrate/20120623020457_add_user_to_textures.rb
+++ db/migrate/20120623020457_add_user_to_textures.rb
@@ -1,6 +1,6 @@
 class AddUserToTextures < ActiveRecord::Migration
   def change
-    add_column :textures, :user_id, :string
+    add_column :textures, :user_id, :integer
     add_index :textures, :user_id
     add_column :textures, :description, :string
   end
