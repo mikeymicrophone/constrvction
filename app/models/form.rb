09d01cc41a29ae8ce263355a4d4184aa1c1ab9e8
Mary Huang <rhymeandreason1.0@gmail.com>
2012-06-27 23:09:06 -0400

    using filesystem instead of s3 for form upload

--- app/models/form.rb
+++ app/models/form.rb
@@ -3,36 +3,19 @@ class Form < ActiveRecord::Base
   attr_accessible :mesh, :name, :image, :front_mesh, :back_mesh
 
   has_attached_file :front_mesh,
-  :storage => :s3,
-  :s3_credentials => "#{Rails.root}/config/s3.yml",
-  #:path => ":class/:attachment/:id/:filename",
-  :s3_host_alias => 's3.constrvct.com',
-  :bucket => 's3.constrvct.com',
-  :url => ":s3_alias_url",
-  :path => "/:class/:id/:filename"
+  :storage => :filesystem
   
   has_attached_file :back_mesh,
-  :storage => :s3,
-  :s3_credentials => "#{Rails.root}/config/s3.yml",
-  #:path => ":class/:attachment/:id/:filename",
-  :s3_host_alias => 's3.constrvct.com',
-  :bucket => 's3.constrvct.com',
-  :url => ":s3_alias_url",
-  :path => "/:class/:id/:filename"
+  :storage => :filesystem
 
   has_attached_file :image, :styles => { :medium => "300x300#", :thumb => "100x100#" },
-  :storage => :s3,
-  :s3_credentials => "#{Rails.root}/config/s3.yml",
-  :s3_host_alias => 's3.constrvct.com',
-  :bucket => 's3.constrvct.com',
-  :url => ":s3_alias_url",
-  :path => "/:class/:id/:basename.:style.:extension",
-  :default_url => '/images/missing_:style.jpg'
+  :storage => :filesystem
 
-=begin  
+=begin
   has_attached_file :front_mesh,
   :storage => :s3,
   :s3_credentials => "#{Rails.root}/config/s3.yml",
+  :s3_headers => { 'Expires' => 1.year.from_now.httpdate },
   #:path => ":class/:attachment/:id/:filename",
   :s3_host_alias => 's3.constrvct.com',
   :bucket => 's3.constrvct.com',
@@ -42,6 +25,7 @@ class Form < ActiveRecord::Base
   has_attached_file :back_mesh,
   :storage => :s3,
   :s3_credentials => "#{Rails.root}/config/s3.yml",
+  :s3_headers => { 'Expires' => 1.year.from_now.httpdate },
   #:path => ":class/:attachment/:id/:filename",
   :s3_host_alias => 's3.constrvct.com',
   :bucket => 's3.constrvct.com',
@@ -51,11 +35,13 @@ class Form < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "300x300#", :thumb => "100x100#" },
   :storage => :s3,
   :s3_credentials => "#{Rails.root}/config/s3.yml",
+  :s3_headers => { 'Expires' => 1.year.from_now.httpdate },
   :s3_host_alias => 's3.constrvct.com',
   :bucket => 's3.constrvct.com',
   :url => ":s3_alias_url",
-  :path => "/:class/:id/:basename.:style.:extension",
+  :path => "/:class/:id/:basename_:style.:extension",
   :default_url => '/images/missing_:style.jpg'
-=end  
+
+=end
   
 end
