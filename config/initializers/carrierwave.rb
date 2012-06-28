c88b1b29b581216f336d5e94161767e104585087
Mike Schwab <mike.schwab@gmail.com>
2012-06-28 17:14:19 -0400

    store s3 credentials in ENV rather than yml file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => S3_ACCESS_KEY_ID,
-    :aws_secret_access_key  => S3_SECRET_ACCESS_KEY
+    :aws_access_key_id      => ENV['S3_ACCESS_KEY_ID'],
+    :aws_secret_access_key  => ENV['S3_SECRET_ACCESS_KEY']
   }
   config.fog_directory  = "constrvct_#{Rails.env}"
 end
